b2aab67d413567af98f291059bc8d8c97af322e4
Johnatan Dias <johnatan.dias@gmail.com>
2020-07-15 19:40:28 -0300

    fix: JSX files issue

--- bitbucket-code-highlight.user.js
+++ bitbucket-code-highlight.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bitbucket code highlight
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Add syntax highlight on bitbucket pull requests
 // @author       Johnatan Dias
 // @match        https://bitbucket.org/**/*
@@ -12,6 +12,9 @@
 
 (function () {
   'use strict';
+
+  const ENABLE_DEBUG = false;
+
   const style = document.createElement('link');
   style.rel = 'stylesheet';
   style.href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/default.min.css';
@@ -21,55 +24,66 @@
   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js';
   document.body.appendChild(script);
 
+  const DEBUG = {
+    LOG: message => ENABLE_DEBUG && console.log('=>', message),
+  };
+
   const highlightCode = () => {
     const notHighlightedCode = document.querySelectorAll(
       '.code-diff:not([data-highlighted="true"])'
     );
 
     if (!notHighlightedCode.length) return;
+    DEBUG.LOG({ notHighlightedCode });
 
     const articleFilesElements = document.querySelectorAll(
       '[data-qa="pr-diff-file-styles"]'
     );
 
     if (!articleFilesElements.length) return;
+    DEBUG.LOG({ articleFilesElements });
 
     addEventListenerOnShowMoreButton();
 
     const handleExtensionName = extension => {
-      const extensions = {
-        jsx: 'js',
-        json: 'js',
-      }
-
+      const extensions = { json: 'js' };
       return extensions[extension] || extension;
     }
 
-    for (let file of articleFilesElements) {
+    Array.from(articleFilesElements).forEach(file => {
       const fileTitle = file.querySelector('[data-qa="bk-filepath"]')
         .firstChild
         .lastElementChild
         .innerText;
 
       const language = handleExtensionName(fileTitle.split('.').pop());
+      DEBUG.LOG({ fileTitle, language });
 
       const linesCode = file.querySelectorAll(
         '.code-diff:not([data-highlighted="true"])'
       );
 
-      for (let codeElement of linesCode) {
-        const code = codeElement.innerHTML;
-        const highlighted = hljs.highlight(language, code);
-        codeElement.innerHTML = highlighted.value;
-        codeElement.dataset.highlighted = true;
-      }
-    }
+      DEBUG.LOG({ linesCode });
+
+      Array.from(linesCode).forEach(codeElement => {
+        const parentElement = codeElement.parentElement;
+        parentElement.classList.add(language);
+        parentElement.style.display = 'flex';
+        parentElement.style.alignItems = 'center';
+
+        hljs.highlightBlock(codeElement);
+        codeElement.style.backgroundColor = 'transparent';
+        codeElement.style.padding = '0';
+      });
+    });
   }
 
   const addEventListenerOnShowMoreButton = () => {
     const buttons = document.querySelectorAll(
       '[data-qa="pr-diff-show-more-lines"]:not([data-event-added="true"])'
     );
+
+    DEBUG.LOG({ buttons });
 
     for (let button of buttons) {
       button.addEventListener('click', highlightCode, false);
